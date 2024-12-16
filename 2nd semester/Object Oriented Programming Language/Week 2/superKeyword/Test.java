ea66882dc9694d8069bb4d899847cd2edaba854d
Shahed Hossain Limon <shahedhossain529@gmail.com>
2024-12-17 03:12:40 +0600

    Add files via upload

---
+++ 2nd semester/Object Oriented Programming Language/Week 2/superKeyword/Test.java
@@ -0,0 +1,10 @@
+
+package superKeyword;
+
+public class Test {
+    public static void main(String[] args) {
+        car Lambo = new car("Black",350,5);
+        Lambo.attribute();
+        
+    }
+}
