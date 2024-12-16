d1e632baa2fd2bf97ac95047395c67984b809f97
Shahed Hossain Limon <shahedhossain529@gmail.com>
2024-12-16 19:44:55 +0600

    Add files via upload

---
+++ 2nd Semester/Object Oriented Programming Language Lab/Week 1/ArithmeticOperations.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+      
+        int a = -5 + 8 * 6;
+        int b = (55 + 9) % 9;
+        double c = 20 + -3 * 5 / 8; 
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+
+        
+        System.out.println("a. -5 + 8 * 6 = " +  a);
+        System.out.println("b. (55+9) % 9 = " + b);
+        System.out.println("c. 20 + -3*5 / 8 = " + c);
+        System.out.println("d. 5 + 15 / 3 * 2 - 8 % 3 = " + d);
+    }
+}
