e1c931123f1fade3685cc6f663f73e5c6462535c
Shahed Hossain Limon <shahedhossain529@gmail.com>
2024-12-17 03:03:56 +0600

    Add files via upload

---
+++ 2nd semester/Object Oriented Programming Language/Week 2/interface/person.java
@@ -0,0 +1,12 @@
+
+package inheritance;
+
+public class person {
+    String name;
+    int age;
+    
+    void displayInformation1(){
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+}
