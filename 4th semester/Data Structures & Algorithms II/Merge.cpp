18481d2e7daca0499ada33929264a3ec5b4ebb41
Shahed Hossain Limon <shahedhossain529@gmail.com>
2025-08-30 09:51:02 +0600

    Update Merge.cpp

--- 4th semester/Data Structures & Algorithms II/Merge.cpp
+++ 4th semester/Data Structures & Algorithms II/Merge.cpp
@@ -1,80 +1,54 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-void printArray(vector<int> &v)
-{
-    for(int i = 0; i<v.size(); i++)
-    {
-        cout<<v[i]<<" ";
+void merge(vector<int> &arr, int st, int mid, int end) {
+     vector<int> temp;
+     int i = st, j = mid+1;
+
+     while(i <= mid && j <= end) {
+        if(arr[i] <= arr[j]) {
+        temp.push_back(arr[i]);
+        i++;
+     } else {
+        temp.push_back(arr[j]);
+        j++;
+       }
+      }
+      while (i <= mid) {
+        temp.push_back(arr[i]);
+        i++;
+      }
+
+      while (j <= end) {
+        temp.push_back(arr[j]);
+        j++;
+      }
+
+      for (int idx=0; idx<temp.size(); idx++) {
+        arr[idx+st] = temp[idx];
+      }
+
+    }
+
+void mergeSort(vector<int> &arr, int st, int end) {
+    if (st < end) {
+        int mid = st + (end-st)/2;
+
+        mergeSort(arr, st, mid); //left half
+        mergeSort(arr, mid+1, end); //Right half
+
+        merge(arr, st, mid, end);
+
+    }
+}
+
+int main () {
+    vector<int> arr= {12, 31, 35, 8, 32, 17};
+    mergeSort(arr, 0, arr.size()-1);
+    for (int val : arr){
+        cout<<val<<" ";
     }
     cout<<endl;
-}
-
-void mergeArray(vector<int> &v,int low,int mid,int high)
-{
-    int leftArraySize = mid - low +1;
-    int rightArraySize = high - mid;
-    vector<int> vleft(leftArraySize), vright(rightArraySize);
-    // copying to the left array
-    for(int i=low; i<=mid;i++)
-    {
-        vleft[i-low] = v[i];
-    }
-    // copying to the right array
-    for(int i=mid+1; i<=high;i++)
-    {
-        vright[i-mid-1] = v[i];
-    }
-
-    int leftPoint = 0, rightPoint = 0, arrayPos = low;
-    while((leftPoint<leftArraySize) && (rightPoint < rightArraySize))
-    {
-        if(vleft[leftPoint] <= vright[rightPoint])
-        {
-            v[arrayPos] = vleft[leftPoint];
-            leftPoint++;
-        }
-        else{
-            v[arrayPos] = vright[rightPoint];
-            rightPoint++;
-        }
-        arrayPos++;
-    }
-    //printArray(v);
-    while(leftPoint<leftArraySize)
-    {
-        v[arrayPos] = vleft[leftPoint];
-        leftPoint++;
-        arrayPos++;
-    }
-    while(rightPoint<rightArraySize)
-    {
-        v[arrayPos] = vright[rightPoint];
-        rightPoint++;
-        arrayPos++;
-    }
-
-}
-
-void merge_sort(vector<int> &v,int low, int high)
-{
-    if(low < high)
-    {
-        int mid = low + (high - low)/2 ;
-        merge_sort(v,low,mid);
-        merge_sort(v,mid+1,high);
-        mergeArray(v,low,mid,high);
-    }
-}
-
-
-
-int main()
-{
-    vector<int> v = {10, 11, 5 , 5, 13, 1, 45, 15, 7, 55, 69};
-    printArray(v);
-    merge_sort(v, 0, v.size()-1);
-    cout<<"After Sorting the Array: ";
-    printArray(v);
-
+    return 0;
 }
